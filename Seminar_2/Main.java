b4548243499c2f2419acb1e065e2bdd62f467387
Boris Shapovalov <bashap@bk.ru>
2023-01-21 02:35:45 +0300

    mod file

--- Seminar_2/Main.java
+++ Seminar_2/Main.java
@@ -3,13 +3,20 @@ package Seminar_2;
 public class Main {
     public static void main(String[] args) {
         Zoo zoo1 = new Zoo();
-        zoo1.addAnimal(new Cat("Васька",1,"серый"))
-        .addAnimal(new Horse("Серебряный", 25));
+        zoo1.addAnimal(new Cat("Васька", 1, "серый"))
+                .addAnimal(new Horse("Серебряный", 25));
 
-        for (Animal an: zoo1.getAnimals()) {
+        for (Animal an : zoo1.getAnimals()) {
             System.out.println(an);
             System.out.println(an.voice());
         }
+
+        System.out.println("Многообразие звуков в зоопарке:");
+
+        for (Speakable speak : zoo1.getSpeakables()) {
+            System.out.println(speak.voice());
+
+        }
     }
-    
+
 }
