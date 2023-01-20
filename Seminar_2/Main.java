d4ce4db8807855847e11d251b7e521c703a83261
Boris Shapovalov <bashap@bk.ru>
2023-01-21 01:26:57 +0300

    mod file

--- Seminar_2/Main.java
+++ Seminar_2/Main.java
@@ -1,5 +1,15 @@
 package Seminar_2;
 
 public class Main {
+    public static void main(String[] args) {
+        Zoo zoo1 = new Zoo();
+        zoo1.addAnimal(new Cat("Васька",1,"серый"))
+        .addAnimal(new Horse("Серебряный", 25));
+
+        for (Animal an: zoo1.getAnimals()) {
+            System.out.println(an);
+            System.out.println(an.voice());
+        }
+    }
     
 }
