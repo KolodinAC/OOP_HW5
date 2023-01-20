3f3a61adf688ef06ae75b43cbac36660b5e7dec5
Boris Shapovalov <bashap@bk.ru>
2023-01-21 00:46:21 +0300

    mod file

--- Seminar_2/Horse.java
+++ Seminar_2/Horse.java
@@ -1,5 +1,15 @@
 package Seminar_2;
 
 public class Horse extends Animal {
+
+    public Horse(String name, int box) {
+        super(name, box);
+    }
+
+    @Override
+    public String voice() {
+        return "И-ГО-ГО";
+    }
+    
     
 }
