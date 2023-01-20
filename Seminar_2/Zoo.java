c445cc5a187e74e6598f58af58433a7ed122ef4c
Boris Shapovalov <bashap@bk.ru>
2023-01-21 02:37:26 +0300

    mod file

--- Seminar_2/Zoo.java
+++ Seminar_2/Zoo.java
@@ -1,9 +1,11 @@
 package Seminar_2;
-import java.util.ArrayList;;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Zoo {
     private ArrayList<Animal> animals;
-    
+    private Radio radio = new Radio();
+
     public Zoo() {
         animals = new ArrayList<Animal>();
     }
@@ -12,7 +14,18 @@ public class Zoo {
         animals.add(a);
         return this;
     }
+
+    public List<Speakable> getSpeakables(){
+        List<Speakable> answer = new ArrayList<Speakable>();
+        for (Animal an  : animals) {
+            answer.add(an);
+        }
+        answer. add(radio);
+        return answer;
+
+    }
     public ArrayList<Animal> getAnimals() {
             return animals;
         }
+
 }
