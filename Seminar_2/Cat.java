eb4f9ad9e11b9c463d3ef196a726392b380fd79a
Boris Shapovalov <bashap@bk.ru>
2023-01-21 01:42:38 +0300

    mod file

--- Seminar_2/Cat.java
+++ Seminar_2/Cat.java
@@ -11,7 +11,12 @@ public class Cat extends Animal{
 
     @Override
     public String voice() {
-        return "Мяу";
+        return "Издает звук: Мяу";
+    }
+
+    @Override
+    public String toString() {
+        return "Кошка " + super.toString();
     }
 
     
