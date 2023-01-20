3c17c954d2a7688f35fe519e6066fd01c6b10d55
Boris Shapovalov <bashap@bk.ru>
2023-01-21 00:27:08 +0300

    modified file

--- Seminar_2/Animal.java
+++ Seminar_2/Animal.java
@@ -1,5 +1,19 @@
 package Seminar_2;
 
-public class Animal {
+public abstract class Animal {
+
+    private String name;
+    private int box;
+
+    public Animal(String name, int box) {
+        this.name = name;
+        this.box = box;
+    }
     
+    @Override
+    public String toString() {
+        return String.format("Name is %s box num: %d", this.name, this,box);
+    }
+
+    public abstract String voice();
 }
