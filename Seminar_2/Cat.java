56da44861ed6c1079bfbd99f22415ca0eddc5a75
Boris Shapovalov <bashap@bk.ru>
2023-01-21 00:39:06 +0300

    modified file

--- Seminar_2/Cat.java
+++ Seminar_2/Cat.java
@@ -1,5 +1,20 @@
 package Seminar_2;
 
-public class Cat {
+public class Cat extends Animal{
+
+    private String color;
+
+    public Cat(String name, int box, String color) {
+        super(name, box);
+        this.color = color;
+    }
+
+    @Override
+    public String voice() {
+        return "Мяу";
+    }
+
+    
+    
     
 }
