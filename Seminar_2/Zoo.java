26e167d6971d780fb850a514c9240f736aee922c
Boris Shapovalov <bashap@bk.ru>
2023-01-21 00:58:12 +0300

    mod file

--- Seminar_2/Zoo.java
+++ Seminar_2/Zoo.java
@@ -1,5 +1,18 @@
 package Seminar_2;
+import java.util.ArrayList;;
 
 public class Zoo {
+    private ArrayList<Animal> animals;
     
+    public Zoo() {
+        animals = new ArrayList<Animal>();
+    }
+
+    public Zoo addAnimal (Animal a) {
+        animals.add(a);
+        return this;
+    }
+    public ArrayList<Animal> getAnimals() {
+            return animals;
+        }
 }
