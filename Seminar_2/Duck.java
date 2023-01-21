03f2a20cc1561208108cabdd2c784552d2d610d5
Boris Shapovalov <bashap@bk.ru>
2023-01-21 05:18:11 +0300

    mod file

--- Seminar_2/Duck.java
+++ Seminar_2/Duck.java
@@ -1,6 +1,6 @@
 package Seminar_2;
 
-public class Duck extends Animal implements Runable, Flyable {
+public class Duck extends Animal implements Runable, Flyable, Swimable {
 
     public Duck(String name, int box) {
         super(name, box);
@@ -13,13 +13,19 @@ public class Duck extends Animal implements Runable, Flyable {
 
     @Override
     public String voice() {
-        return "Кря - Кря";
+        return "Издает звук: Кря - Кря";
     }
 
     @Override
     public int speedofRun() {
         
         return 5;
+    }
+
+    @Override
+    public int speedofSwim() {
+        
+        return 12;
     }
     
 }
