9a8fc2238fdca0017332396c6a9bfb6bc1985e74
Boris Shapovalov <bashap@bk.ru>
2023-01-21 04:24:13 +0300

    mod file

--- Seminar_2/Main.java
+++ Seminar_2/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Zoo zoo1 = new Zoo();
         zoo1.addAnimal(new Cat("Васька", 1, "серый"))
-                .addAnimal(new Horse("Серебряный", 25));
+                .addAnimal(new Horse("Серебряный", 25))
+                .addAnimal(new Duck("Утя", 17));
 
         for (Animal an : zoo1.getAnimals()) {
             System.out.println(an);
             System.out.println(an.voice());
         }
+
+        System.out.println("-------------------");
 
         System.out.println("Многообразие звуков в зоопарке:");
 
@@ -17,6 +20,23 @@ public class Main {
             System.out.println(speak.voice());
 
         }
+
+        System.out.println("-------------------");
+
+        for (Runable run : zoo1.getRunables()) {
+            System.out.println("Бегает со скоростью:" + run.speedofRun());
+            
+        }
+
+        int max = zoo1.getMaxspeed();
+        System.out.println(String.format("Максимальная скорость которую развивают животные: %d", max));
+
+        System.out.println("-------------------");
+        for (Flyable flyable : zoo1.getFLyers()) {
+            System.out.printf("Cкорость полета : %d", flyable.speedofFly());
+        }
+
+
     }
 
 }
